505bd0f8823e0375e45b6cfcc37a920c919fb235
raihaanalif <68364765+raihaanalif@users.noreply.github.com>
2020-07-24 00:21:41 +0700

    Update selfQuestion.blade.php

--- resources/views/selfQuestion.blade.php
+++ resources/views/selfQuestion.blade.php
@@ -1,23 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-    <h1>List Pertanyaan yang Telah Anda Post</h1>
-    <table>
-        <thead>
-            <th>No</th>
-            <th>Pertanyaan</th>
-            <th>Aksi</th>
-        </thead>
-        <tbody>
-            @foreach($question as $key => $question)
-            <tr>
+<div class="container">
+  <div class="row justify-content-center mt-5">
+    <div class="col-md-10">
+      <div class="card">
+        <div class="card-header bg-transparent mb-0">
+          <h3 class="text-center">List Pertanyaan yang Telah Anda Post</h3>
+        </div>
+        <div class="card-body">
+          <table class="table table-striped">
+            <thead class="text-left thead-dark">
+              <th>No</th>
+              <th class="col-xl">Pertanyaan</th>
+              <th>Aksi</th>
+            </thead>
+            <tbody>
+              @foreach($question as $key => $question)
+              <tr class="text-left">
                 <td>{{$key+1}}</td>
                 <td>{{$question->pertanyaan}}</td>
-                <td><a class="btn btn-primary" href="{{route('detailQuestion', $question->id_question)}}">Lihat</a></td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+                <td><a class="btn btn-primary" href="{{route('selfDetailQuestion', $question->id_question)}}">Lihat</a></td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
