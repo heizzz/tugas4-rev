eeeb7dfef356eb0ff4e557adad1f241f85ebb3dc
raihaanalif <68364765+raihaanalif@users.noreply.github.com>
2020-07-24 00:22:24 +0700

    Update question.blade.php

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -1,32 +1,40 @@
 @extends('layouts.app')
 @section('content')
-<a href="{{route('sort')}}">Urutkan Berdasarkan Tanggal</a>
-<form method="POST" action="{{route('addQuestion')}}">
+<div class="container">
+  <a href="{{route('sort')}}">Urutkan Berdasarkan Tanggal</a>
+  <form method="POST" action="{{route('addQuestion')}}">
     <div class="form-group">
-        @csrf
-        <label>Pertanyaan Baru</label>
-        <input type="text" name="question" class="form-control">
+      @csrf
+      <label>Pertanyaan Baru</label>
+      <input type="text" name="question" class="form-control">
     </div>
-    <a class="btn btn-primary">Tambah Pertanyaan</a>
-</form>
-
-<h2>List Pertanyaan</h2>
-<table>
-    <thead>
-        <th>Nama</th>
-        <th>Tanggal</th>
-        <th>Pertanyaan</th>
-        <th>Aksi</th>
-    </thead>
-    <tbody>
-        @foreach($question as $key => $question)
-        <tr>
-            <td>{{$question->id_user}}</td>
-            <td>{{$question->updated_at}}</td>
-            <td>{{$question->pertanyaan}}</td>
-            <td><a class = "btn btn-primary" href="{{route('detailQuestion', $question->id_question)}}">Lihat</a></td>
-        </tr>
-        @endforeach
-    </tbody>
-<table>
+    <a class="btn btn-primary" align="center">Tambah Pertanyaan</a>
+  </form>
+  <div class="row justify-content-center mt-5">
+    <div class="col-md-10">
+      <div class="card">
+        <div class="card-header bg-transparent mb-0">
+          <h3 class="text-center">List Pertanyaan</h3>
+          </div>
+        <div class="card-body">
+          <table class="table table-striped">
+            <thead class="text-left thead-dark">
+              <th>Tanggal</th>
+              <th class="col-xl">Pertanyaan</th>
+              <th>Aksi</th>
+            </thead>
+            <tbody>
+              @foreach($question as $key => $question)
+              <tr class="text-left">
+                <td>{{$question->updated_at}}</td>
+                <td>{{$question->pertanyaan}}</td>
+                <td><a class = "btn btn-primary" href="{{route('detailQuestion', $question->id_question)}}">Lihat</a></td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
