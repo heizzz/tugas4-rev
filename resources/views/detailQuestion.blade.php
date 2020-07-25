6b5debb5cd875ab125019c57c7c8fc6d76a0fea7
ThomasFelix <37539546+ThomasFel@users.noreply.github.com>
2020-07-25 17:36:20 +0700

    Update detailQuestion.blade.php

--- resources/views/detailQuestion.blade.php
+++ resources/views/detailQuestion.blade.php
@@ -11,17 +11,15 @@
         <div class="card-body">
           <table class="table table-striped">
             <thead class="text-left thead-dark">
-              <!-- <th>No</th> -->
-              <th>Nama</th>
-              <th>Time</th>
-              <th class="col col-xl">Pertanyaan</th>
+              <th style="width:100px">Waktu Post</th>
+              <th style="width:150px">Nama</th>
+              <th style="width:500px">Pertanyaan</th>
             </thead>
             <tbody>
               <tr class="text-left">
                 @foreach ($question as $key => $question)
-                <!-- <td>{{$key+1}}</td> -->
-                <td>{{$question->name}}</td>
                 <td>{{$question->updated_at}}</td>
+                <td>{{$question->name}}</td>
                 <td>{{$question->pertanyaan}}</td>
               </tr>
             </tbody>
@@ -36,36 +34,36 @@
     <div class="col-md-10">
       <div class="card">
         <div class="card-header bg-transparent mb-0">
-          <h3 class="text-center">Jawaban</h3>
+          <h3 class="text-center">List Jawaban</h3>
         </div>
         <div class="card-body">
           <table class="table table-striped">
             <thead class="text-left thead-dark">
-              <th>Nama</th>
-              <th>Time</th>
-              <th class="col col-xl">Answer</th>
+              <th style="width:150px">Nama</th>
+              <th style="width:100px">Waktu Post</th>
+              <th style="width:450px">Jawaban</th>
             </thead>
             <tbody>
               @foreach ($answer as $key => $answer)
               <tr class="text-left">
                 <td>{{$answer->name}}</td>
                 <td>{{$answer->updated_at}}</td>
-                <td >{{$answer->jawaban}}</td>
+                <td>{{$answer->jawaban}}</td>
               </tr>
               @endforeach
             </tbody>
           </table>
         </div>
-      </div>
-        <form method="POST" action="{{route('addAnswer')}}">
+        <form method="POST" action="{{route('addAnswer')}}" style="width:600px; margin: 20px 0  20px 20px">
           <input type="hidden" value="{{$question->id_question}}" name="id_question">
           <div class="form-group">
             @csrf
             <label>Jawab</label>
-            <input type="text" name="answer" class="form-control">
+            <input type="text" name="answer" class="form-control mr-sm-2">
           </div>
           <button type="submit" class="btn btn-primary">Tambah Jawaban</button>
         </form>
+      </div>
     </div>
   </div>
 
