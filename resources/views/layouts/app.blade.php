ccaf9c6a99b5e45aee402cfab04d61c8abe900c3
raihaanalif <68364765+raihaanalif@users.noreply.github.com>
2020-07-25 13:57:44 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,11 +60,11 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-                                    <a class="dropdown-item" href="{{route('home')}}">Home</a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
+                                    <a class="dropdown-item" href="{{route('home')}}">Home</a>
                                 </div>
                             </li>
                         @endguest
