746554cbe9442330805d2841891bb05baf117f96
ThomasFelix <37539546+ThomasFel@users.noreply.github.com>
2020-07-25 17:34:15 +0700

    Update selfDetailAnswer.blade.php

--- resources/views/selfDetailAnswer.blade.php
+++ resources/views/selfDetailAnswer.blade.php
@@ -6,19 +6,21 @@
     <div class="col-md-10">
       <div class="card">
         <div class="card-header bg-transparent mb-0">
-          <h3 class="text-center">Detail Pertanyaan</h3>
+          <h3 class="text-center">Pertanyaan</h3>
         </div>
         <div class="card-body">
           <table class="table table-striped">
             <thead class="text-left thead-dark">
-              <th>No</th>
-              <th>Nama</th>
-              <th class="col col-xl">Pertanyaan</th>
+              <th style="width:20px">No</th>
+              <th style="width:150px">Waktu Post</th>
+              <th style="width:150px">Nama</th>
+              <th style="width:500px">Pertanyaan</th>
             </thead>
             <tbody>
               <tr class="text-left">
                 @foreach ($question as $key => $q)
                 <td>{{$key + $question->firstItem()}}</td>
+                <td>{{$q->updated_at}}</td>
                 <td>{{$q->name}}</td>
                 <td>{{$q->pertanyaan}}</td>
               </tr>
@@ -38,23 +40,25 @@
     <div class="col-md-10">
       <div class="card">
         <div class="card-header bg-transparent mb-0">
-          <h3 class="text-center">Jawaban</h3>
+          <h3 class="text-center">Detail Jawaban</h3>
         </div>
         <div class="card-body">
           <table class="table table-striped">
             <thead class="text-left thead-dark">
-              <th>Time</th>
-              <th class="col col-xl">Answer</th>
-              <th>Aksi</th>
+              <th style="width:20px">No</th>
+              <th style="width:100px">Waktu Post</th>
+              <th style="width:450px">Jawaban</th>
+              <th style="width:90px">Tindakan</th>
             </thead>
             <tbody>
               @foreach ($answer as $key => $a)
               <tr class="text-left">
+                <td>{{$key + $answer->firstItem()}}</td>
                 <td>{{$a->updated_at}}</td>
                 <td>{{$a->jawaban}}</td>
                 <td>
                   <a href="{{route('edit_answer', $a->id_answer)}}" class="btn btn-warning btn-block">Edit</a>
-                  <a href="{{route('delete_answer', $a->id_answer)}}" class="btn btn-danger btn-block">Delete</a>
+                  <a href="{{route('delete_answer', $a->id_answer)}}" onclick="return confirm('Yakin dihapus?')"class="btn btn-danger btn-block">Hapus</a>
                 </td>
               </tr>
               @endforeach
