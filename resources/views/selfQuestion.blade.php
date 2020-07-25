e911dbedf5d685201304686f764938c2d439daae
ThomasFelix <37539546+ThomasFel@users.noreply.github.com>
2020-07-25 12:37:46 +0700

    Update selfQuestion.blade.php

--- resources/views/selfQuestion.blade.php
+++ resources/views/selfQuestion.blade.php
@@ -8,6 +8,12 @@
         <div class="card-header bg-transparent mb-0">
           <h3 class="text-center">List Pertanyaan yang Telah Anda Post</h3>
         </div>
+        <form action="{{route('selfQuestion')}}" method="GET" class="navbar-form navbar-left" style="width:500px; margin: 20px 0  0 20px; float:right">
+          <div class="input-group">
+            <input name="search" type="search" aria-label="Search" class="form-control mr-sm-2" placeholder="Search">
+            <button type="submit" class="btn btn-primary">Cari</button></span>
+          </div>
+        </form>
         <div class="card-body">
           <table class="table table-striped">
             <thead class="text-left thead-dark">
@@ -17,16 +23,19 @@
               <th>Aksi</th>
             </thead>
             <tbody>
-              @foreach($question as $key => $question)
+              @foreach($question as $key => $q)
               <tr class="text-left">
                 <td>{{$key+1}}</td>
-                <td>{{$question->updated_at}}</td>
-                <td>{{$question->pertanyaan}}</td>
-                <td><a class="btn btn-primary" href="{{route('selfDetailQuestion', $question->id_question)}}">Lihat</a></td>
+                <td>{{$q->updated_at}}</td>
+                <td>{{$q->pertanyaan}}</td>
+                <td><a class="btn btn-primary" href="{{route('selfDetailQuestion', $q->id_question)}}">Lihat</a></td>
               </tr>
               @endforeach
             </tbody>
           </table>
+
+          {{$question->links()}}
+
         </div>
       </div>
     </div>
