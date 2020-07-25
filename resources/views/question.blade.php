3e32af643c119595e1b913ff94291db04178163b
ThomasFelix <37539546+ThomasFel@users.noreply.github.com>
2020-07-25 17:35:42 +0700

    Update question.blade.php

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -1,15 +1,9 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-  <a href="{{route('sort')}}">Urutkan Berdasarkan Tanggal</a>
-  <form method="POST" action="{{route('addQuestion')}}">
-    <div class="form-group">
-      @csrf
-      <label>Pertanyaan Baru</label>
-      <input type="text" name="question" class="form-control">
-    </div>
-    <button type="submit" class="btn btn-primary" align="center">Tambah Pertanyaan</button>
-  </form>
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+    Tambah Pertanyaan Baru
+  </button>
   <div class="row justify-content-center mt-5">
     <div class="col-md-10">
       <div class="card">
@@ -22,21 +16,22 @@
             <button type="submit" class="btn btn-primary">Cari</button></span>
           </div>
         </form>
+        <a href="{{route('sort')}}" style="margin: 0 0 0 730px;">Urut Berdasarkan Tanggal</a>
         <div class="card-body">
           <table class="table table-striped">
             <thead class="text-left thead-dark">
-              <th>No</th>
-              <th>Nama</th>
-              <th>Tanggal</th>
-              <th class="col-xl">Pertanyaan</th>
-              <th>Aksi</th>
+              <th style="width:20px">No</th>
+              <th style="width:100px">Waktu Post</th>
+              <th style="width:150px">Nama</th>
+              <th style="width:450px">Pertanyaan</th>
+              <th style="width:90px">Tindakan</th>
             </thead>
             <tbody>
               @foreach($question as $key => $q)
               <tr class="text-left">
                 <td>{{$key + $question->firstItem()}}</td>
-                <td>{{$q->name}}</td>
                 <td>{{$q->updated_at}}</td>
+                <td>{{$q->name}}</td>
                 <td>{{$q->pertanyaan}}</td>
                 <td><a class = "btn btn-primary" href="{{route('detailQuestion', $q->id_question)}}">Lihat</a></td>
               </tr>
@@ -50,4 +45,30 @@
       </div>
     </div>
   </div>
+  <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Tambah Pertanyaan Baru</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form method="POST" action="{{route('addQuestion')}}">
+          <div class="form-group">
+            @csrf
+            <label for="exampleFormControlTextarea1">Pertanyaan</label>
+            <input type="text" name="question" class="form-control" required></textarea>
+          </div>
+      <div class="modal-footer">
+          <button type="submit" class="btn btn-primary">Tambah</button>
+        </form>
+      </div>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
